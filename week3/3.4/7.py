9acb47991d204a620c0990ca1a3c810863996abd
Yakov <yakovburtsev@gmail.com>
2017-08-24 10:56:40 +0300

    3.4.7 not passed yet

--- week3/3.4/7.py
+++ week3/3.4/7.py
@@ -3,12 +3,12 @@ import re
 import requests
 
 html_link = input().strip()
+# html_link = 'https://stepic.org/media/attachments/lesson/24471/03'
 
 
 def f(links, pattern, strip_char, results):
     for lst in [re.findall(pattern, x, flags=re.IGNORECASE) for x in links]:
         for link in lst:
-            # print(link)
             link = link.strip(strip_char)
             if link.startswith(".."):
                 continue
@@ -24,10 +24,8 @@ def f(links, pattern, strip_char, results):
 text = requests.get(html_link).text
 
 results = set()
-links1 = re.findall(r"<a.*?href='.+?'>", text, flags=re.IGNORECASE)
-links2 = re.findall(r'<a.*?href=".+?">', text, flags=re.IGNORECASE)
-# print(links1)
-# print(links2)
+links1 = re.findall(r"<a.*?href='.+?'.*?>", text, flags=re.IGNORECASE)
+links2 = re.findall(r'<a.*?href=".+?".*?>', text, flags=re.IGNORECASE)
 f(links1, r"href='.+?'", "'", results)
 f(links2, r'href=".+?"', '"', results)
 
