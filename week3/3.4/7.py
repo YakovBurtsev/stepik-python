d8f28c966187462a8c13a9571074ba0a1280facb
Yakov <yakovburtsev@gmail.com>
2017-08-24 10:46:08 +0300

    3.4.7 v2

--- week3/3.4/7.py
+++ week3/3.4/7.py
@@ -5,26 +5,31 @@ import requests
 html_link = input().strip()
 
 
-def f(links, strip_char, results):
-    for link in [x.split('=')[1].strip(strip_char) for x in links]:
-        if link.startswith(".."):
-            continue
-        if "://" in link:
-            link = link.split("://")[1]
-        if "/" in link:
-            link = link.split("/")[0]
-        if ":" in link:
-            link = link.split(":")[0]
-        results.add(link)
+def f(links, pattern, strip_char, results):
+    for lst in [re.findall(pattern, x, flags=re.IGNORECASE) for x in links]:
+        for link in lst:
+            # print(link)
+            link = link.strip(strip_char)
+            if link.startswith(".."):
+                continue
+            if "://" in link:
+                link = link.split("://")[1]
+            if "/" in link:
+                link = link.split("/")[0]
+            if ":" in link:
+                link = link.split(":")[0]
+            results.add(link)
+
 
 text = requests.get(html_link).text
 
 results = set()
-links1 = re.findall(r"<a.*href='.*'", text, flags=re.IGNORECASE)
-links2 = re.findall(r'<a.*href=".*"', text, flags=re.IGNORECASE)
-f(links1, "'", results)
-f(links2, '"', results)
+links1 = re.findall(r"<a.*?href='.+?'>", text, flags=re.IGNORECASE)
+links2 = re.findall(r'<a.*?href=".+?">', text, flags=re.IGNORECASE)
+# print(links1)
+# print(links2)
+f(links1, r"href='.+?'", "'", results)
+f(links2, r'href=".+?"', '"', results)
 
-
-for r in results:
+for r in sorted(results):
     print(r)
