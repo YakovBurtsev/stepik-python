edbaecb6f3938b4b8c11575196c95a4065ffcb58
Yakov <yakovburtsev@gmail.com>
2017-08-25 09:45:27 +0300

    3.4.7 done

--- week3/3.4/7.py
+++ week3/3.4/7.py
@@ -2,32 +2,24 @@ import re
 
 import requests
 
-html_link = input().strip()
+html_link = input()
 # html_link = 'https://stepic.org/media/attachments/lesson/24471/03'
-
-
-def f(links, pattern, strip_char, results):
-    for lst in [re.findall(pattern, x, flags=re.IGNORECASE) for x in links]:
-        for link in lst:
-            link = link.strip(strip_char)
-            if link.startswith(".."):
-                continue
-            if "://" in link:
-                link = link.split("://")[1]
-            if "/" in link:
-                link = link.split("/")[0]
-            if ":" in link:
-                link = link.split(":")[0]
-            results.add(link)
-
-
 text = requests.get(html_link).text
 
 results = set()
-links1 = re.findall(r"<a.*?href='.+?'.*?>", text, flags=re.IGNORECASE)
-links2 = re.findall(r'<a.*?href=".+?".*?>', text, flags=re.IGNORECASE)
-f(links1, r"href='.+?'", "'", results)
-f(links2, r'href=".+?"', '"', results)
+links = re.findall("""<a .*?href=['"](.+?)['"].*?>""", text, flags=re.IGNORECASE)
+# print(links)
+for link in links:
+    if link.startswith(".."):
+        continue
+    if "://" in link:
+        link = link.split("://")[1]
+    if "/" in link:
+        link = link.split("/")[0]
+    if ":" in link:
+        link = link.split(":")[0]
+    results.add(link)
+
 
 for r in sorted(results):
     print(r)
